de15b737732c00b36e2c16a9d1de90edade984fe
Sana0601 <239x1a05d9@gprec.ac.in>
2026-04-21 21:19:52 +0530

    Time: 48 ms (66.67%), Space: 112.1 MB (89.25%) - LeetHub

---
+++ 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations.java
@@ -0,0 +1,48 @@
+class Solution {
+    int[] parent;
+    public int minimumHammingDistance(int[] source, int[] target, int[][] allowedSwaps) {
+        int n=source.length;
+        parent =new int[n];
+        for(int i=0;i<n;i++)
+        parent[i]=i;
+        for(int[] swap:allowedSwaps)
+        {
+            union(swap[0],swap[1]);
+        }
+        Map<Integer,Map<Integer,Integer>>map=new HashMap<>();
+        for(int i=0;i<n;i++)
+        {
+            int p=find(i);
+            map.putIfAbsent(p,new HashMap<>());
+            Map<Integer,Integer>freq=map.get(p);
+            freq.put(source[i],freq.getOrDefault(source[i],0)+1);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            int p=find(i);
+            Map<Integer,Integer>freq=map.get(p);
+            if(freq.getOrDefault(target[i],0)>0)
+            {
+                freq.put(target[i],freq.get(target[i])-1);
+            }
+            else
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+    int find(int x)
+    {
+        if(parent[x]!=x)
+        {
+            parent[x]=find(parent[x]);
+        }
+        return parent[x];
+    }
+    void union(int a,int b)
+    {
+        parent[find(a)]=find(b);
+    }
+}
